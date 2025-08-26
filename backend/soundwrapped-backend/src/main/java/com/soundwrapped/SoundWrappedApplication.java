fbee4bafd4677d9105dfedec486256c2d36765a6
Tazwar Sikder <tazwar_rocks@hotmail.com>
2025-08-26 01:02:52 -0400

    Fixed .env filepath

--- backend/soundwrapped-backend/src/main/java/com/soundwrapped/SoundWrappedApplication.java
+++ backend/soundwrapped-backend/src/main/java/com/soundwrapped/SoundWrappedApplication.java
@@ -8,12 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SoundWrappedApplication {
 	public static void main(String[] args) {
 		//Load .env from backend folder
-		Dotenv dotenv = Dotenv.configure().directory("../").filename(".env").load(); //Relative to soundwrapped-backend/src/main/java
-
-		System.out.println("CLIENT_ID: " + dotenv.get("CLIENT_ID"));
-		System.out.println("CLIENT_SECRET: " + dotenv.get("CLIENT_SECRET"));
-		System.out.println("REDIRECT_URI: " + dotenv.get("REDIRECT_URI"));
-
+		Dotenv dotenv = Dotenv.configure().directory("./").filename(".env").load(); //Relative to soundwrapped-backend/src/main/java
 		SpringApplication.run(SoundWrappedApplication.class, args);
 	}
 }
