321698ff8e1ed47b7e6e73fc1b1887bd6c3ad3b8
Tazwar Sikder <tazwar_rocks@hotmail.com>
2025-08-25 21:38:39 -0400

    minor updates

--- backend/soundwrapped-backend/src/main/java/com/soundwrapped/soundwrapped_backend/SoundWrappedApplication.java
+++ backend/soundwrapped-backend/src/main/java/com/soundwrapped/soundwrapped_backend/SoundWrappedApplication.java
@@ -1,13 +1,23 @@
 package com.soundwrapped.soundwrapped_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SoundWrappedApplication {
-
 	public static void main(String[] args) {
+		//Load .env from backend folder
+		Dotenv dotenv = Dotenv.configure().directory("../").filename(".env").load(); //Relative to soundwrapped-backend/src/main/java
+
+		String clientId = dotenv.get("CLIENT_ID");
+		String clientSecret = dotenv.get("CLIENT_SECRET");
+		String redirectUri = dotenv.get("REDIRECT_URI");
+
+		System.out.println("CLIENT_ID: " + clientId);
+		System.out.println("CLIENT_SECRET: " + clientSecret);
+		System.out.println("REDIRECT_URI: " + redirectUri);
+
 		SpringApplication.run(SoundWrappedApplication.class, args);
 	}
-
 }
