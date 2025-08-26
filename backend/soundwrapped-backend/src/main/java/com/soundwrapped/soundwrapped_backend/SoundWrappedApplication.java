def942c046bf301864aa5e6257951cc9b4378916
Tazwar Sikder <tazwar_rocks@hotmail.com>
2025-08-25 21:00:26 -0400

    Obtained backend setup from Spring Initializr
    Contains settings for Maven, Spring Boot + Java version, dependencies, and project metadata.

---
+++ backend/soundwrapped-backend/src/main/java/com/soundwrapped/soundwrapped_backend/SoundWrappedApplication.java
@@ -0,0 +1,13 @@
+package com.soundwrapped.soundwrapped_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundWrappedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundWrappedApplication.class, args);
+	}
+
+}
---
+++ backend/soundwrapped-backend/src/test/java/com/soundwrapped/soundwrapped_backend/SoundWrappedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundwrapped.soundwrapped_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundWrappedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
